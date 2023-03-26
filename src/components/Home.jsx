cf4997161a3ad6a610bd2d3f33ffe44d72e597e1
Confidence Madiope <confidencem97@gmail.com>
2023-03-26 13:05:04 +0200

    keyframes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1,1, 1.1,1,1.1,1],
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+  },
+  
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -15,11 +24,8 @@ const Home = () => {
       </h2>
       <Link to='/base' className='flex justify-center'>
         <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
+          variants={buttonVariants}
+          whileHover='hover'
           className='text-white py-[10px] px-[30px] rounded-[50px] border-2 border-white cursor-pointer opacity-[0.7]'
         >
           Create You Pizza
